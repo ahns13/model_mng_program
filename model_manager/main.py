5f2d4ba6f882420ec2e91ec32154e82ff428f9bd
안태호 <ahnforcsh@gmail.com>
2022-05-03 22:28:04 +0900

    modify

--- model_manager/main.py
+++ model_manager/main.py
@@ -4,7 +4,7 @@ from PyQt5 import uic
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import *
 
-# from model_sql_ui import *
+from model_sql_ui import *
 
 form_class = uic.loadUiType("./model_manage_main.ui")[0]
 
@@ -13,9 +13,9 @@ class MainWindow(QMainWindow, form_class):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setupUi(self)
+
         # tableWidget
-        self.tableWidget.setSpan(0,0,1,6)
-        self.tableWidget.setItem(0,0, QTableWidgetItem("조회된 데이터가 없습니다."))
+        self.noSearchMsg()
         self.btn_search.clicked.connect(self.getDefaultTableData)
         self.btn_next.clicked.connect(lambda: self.tablePaging("next"))
         self.btn_prev.clicked.connect(lambda: self.tablePaging("prev"))
@@ -23,16 +23,21 @@ class MainWindow(QMainWindow, form_class):
         # modelData
         self.tablePageNo = 1
         self.modelDataLen = 0
+        self.searchName = ""
 
         self.show()
         print(dir(self))
 
     def getDefaultTableData(self):
+        self.searchName = self.lineEdit.text()
         self.tableWidget.removeRow(0)
-        model_data = [[1,2,3,4,5,6], [1,2,3,4,5,6], [1,2,3,4,5,6]]
-        # model_data = get_model_list(self.tablePageNo)
+        # model_data = [[1,2,3,4,5,6], [1,2,3,4,5,6], [1,2,3,4,5,6]]
+        model_data = get_model_list(self.tablePageNo, self.searchName)
         self.modelDataLen = len(model_data)
-        self.modelDataUpdate(model_data)
+        if self.modelDataLen:
+            self.modelDataUpdate(model_data)
+        else:
+            self.noSearchMsg()
 
     def tablePaging(self, v_btn_type):
         if v_btn_type == "next":
@@ -48,16 +53,21 @@ class MainWindow(QMainWindow, form_class):
             else:
                 self.tablePageNo -= 1
 
-        self.tableWidget.setRowCount(0)
-        model_data = [[10,20,30,40,50,60], [99,88,77,66,55,44], [7,6,5,4,3,2]]
-        # model_data = get_model_list(self.tablePageNo)
+        # model_data = [[10,20,30,40,50,60], [99,88,77,66,55,44], [7,6,5,4,3,2]]
+        model_data = get_model_list(self.tablePageNo)
         self.modelDataUpdate(model_data)
 
     def modelDataUpdate(self, v_model_data):
+        self.tableWidget.setRowCount(0)
         for m_idx, m_row in enumerate(v_model_data):
             self.tableWidget.insertRow(m_idx)
             for r_idx, r_data in enumerate(m_row):
-                self.tableWidget.setItem(m_idx, r_idx, QTableWidgetItem(str(r_data)))
+                self.tableWidget.setItem(m_idx, r_idx, QTableWidgetItem(str(r_data if r_data else "")))
+
+    def noSearchMsg(self):
+        self.tableWidget.setRowCount(0)
+        self.tableWidget.setSpan(0, 0, 1, 6)
+        self.tableWidget.setItem(0, 0, QTableWidgetItem("조회된 데이터가 없습니다."))
 
 
 if __name__ == "__main__":
--- model_manager/model_sql_ui.py
+++ model_manager/model_sql_ui.py
@@ -1,4 +1,5 @@
 import importlib
+import sys
 
 cx_Oracle = importlib.import_module("cx_Oracle")
 username = "ADMIN"
@@ -7,14 +8,28 @@ conn = cx_Oracle.connect(user=username, password=password, dsn="modeldb_medium")
 cursor = conn.cursor()
 
 
-def get_model_list(v_page_no, v_input_name=None):
-    check_sql = ("SELECT A.NAME, A.BIRTH_DATE, A.HEIGHT, A.TEL, A.INSTA_ID, A.DATA_DATE"
-                 "  FROM MODEL_PROFILE A"
-                 " WHERE A.NAME " + "LIKE '%" + v_input_name + "%'" if v_input_name else "= '" + v_input_name + "'"
-                 " ORDER BY A.NAME"
-                 "OFFSET 20*(1-" + v_page_no + " ROWS)"
-                 " FETCH NEXT 20 ROWS ONLY")
-    cursor.execute(check_sql)
-    result = cursor.fetchall()
-    return result
+def get_model_list(v_page_no, v_input_name=""):
+    sql = "SELECT A.NAME, A.BIRTH_DATE, A.HEIGHT, A.TEL, A.INSTA_ID, A.DATA_DATE\n"
+    sql += "  FROM MODEL_PROFILE A\n"
+    if v_input_name:
+        sql += " WHERE A.NAME LIKE '%" + v_input_name + "%'\n"
+    sql += " ORDER BY A.NAME\n"
+    sql += "OFFSET 20*(1-" + str(v_page_no) + ") ROWS\n"
+    sql += " FETCH NEXT 20 ROWS ONLY"
+    print(sql)
+    try:
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        return result
+        cursor.close()
+    except cx_Oracle.DatabaseError as e:
+        error, = e.args
+        print(sql)
+        print("error code : " + str(error.code))
+        print(error.message)
+        print(error.context)
+        cursor.close()
+        conn.close()
+        sys.exit()
+
 
